045deff11c3aa9a84fe664d93234412e369dcbdc
Miguel Rodrigues <miguel.b.rodrigues@gmail>
2014-11-20 10:28:53 +0000

    code clean up

--- provisioners/install.sh
+++ provisioners/install.sh
@@ -4,7 +4,5 @@ sudo apt-add-repository "deb $BASE_REPO" >>$LOGFILE
 sudo apt-get update
 sudo apt-get install --force-yes -y bgch-bootstrap >> $LOGFILE 2>&1
 sudo -i /usr/lib/bgch/bootstrap/bootstrapper.sh -p $PASSWORD $IDENTIFICATION >> $LOGFILE 2>&1
-
-
 # force 0 exit status 
 echo "done" > /tmp/bootstrapper.log
